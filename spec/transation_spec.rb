1d0575f47c00802223f71b3ddbcfe8df0b0d2505
Beca-g <rebekahgalliano@gmail.com>
2020-12-01 11:50:16 +0000

    Transactions are initialized with a trasaction amount

--- spec/transation_spec.rb
+++ spec/transation_spec.rb
@@ -1,11 +1,15 @@
 require 'transaction'
 
 describe 'Transaction' do
-  let(:transaction) { Transaction.new(type: 'deposit') }
+  let(:transaction) { Transaction.new(type: 'deposit', amount: 1000) }
 
   describe '#initialize' do
    it 'stores the transaction type' do
     expect(transaction.type).to eq 'deposit'
+   end
+
+   it 'stores the transaction amount' do
+     expect(transaction.amount).to eq 1000
    end
   end
 end
