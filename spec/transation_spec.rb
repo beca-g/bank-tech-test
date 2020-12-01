78127dba386750cc50597de06d6d82e5e5fda34a
Beca-g <rebekahgalliano@gmail.com>
2020-12-01 11:56:53 +0000

    Transactions are initialized with the date it was generated

--- spec/transation_spec.rb
+++ spec/transation_spec.rb
@@ -2,6 +2,7 @@ require 'transaction'
 
 describe 'Transaction' do
   let(:transaction) { Transaction.new(type: 'deposit', amount: 1000) }
+  let(:date) { Time.now.strftime('%d/%m/%Y') }
 
   describe '#initialize' do
    it 'stores the transaction type' do
@@ -10,6 +11,10 @@ describe 'Transaction' do
 
    it 'stores the transaction amount' do
      expect(transaction.amount).to eq 1000
+   end
+
+   it 'stores transactions dates' do
+    expect(transaction.date).to eq date
    end
   end
 end
