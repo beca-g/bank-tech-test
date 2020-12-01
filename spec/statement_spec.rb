795cd60adb80fd66d40146233fb8f6fdc9474986
Beca-g <rebekahgalliano@gmail.com>
2020-12-01 15:10:49 +0000

    Users are able to print statements with deposite transaction history

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -3,6 +3,9 @@ require 'statement'
 describe Statement do
   let(:account) { double :account }
   let(:statement) { Statement.new(account) }
+  let(:date) { Time.now.strftime('%d/%m/%Y') }
+  let(:deposit) { double :transaction, date: date, type: 'deposit', amount: 1000}
+
   describe '#initialize' do
     it 'statement initializes with account' do
       expect(statement.account).to eq account
@@ -10,9 +13,18 @@ describe Statement do
   end
 
   describe '#print' do
-    context 'if not transactions have been made' do
-      it 'returns statement headings only' do
+    let(:account) { double :account, transactions: []}
+    context 'no transactions have been made' do
+      it 'prints statement headings only' do
         expect { statement.print }.to output("date || credit || debit || balance\n").to_stdout
+      end
+    end
+
+    context 'deposit generated' do
+      let(:account) { double :account, transactions: [deposit], balance: 1000 }
+      it 'prints statement with transaction details' do
+        statement_body = "date || credit || debit || balance\n#{date} || 1000.00 || || 1000.00\n"
+        expect { statement.print }.to output(statement_body).to_stdout
       end
     end
   end
